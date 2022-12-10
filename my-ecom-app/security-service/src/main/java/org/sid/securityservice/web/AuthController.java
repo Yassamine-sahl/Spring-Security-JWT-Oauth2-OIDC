1b8b19b65b28480e7c4c94bc210c675cf76f50bb
Yassamine-sahl <yassamine.sahl@gmail.com>
2022-12-10 01:00:30 +0100

    Part 2: Spring Security JWT Oauth2 OIDC

--- my-ecom-app/security-service/src/main/java/org/sid/securityservice/web/AuthController.java
+++ my-ecom-app/security-service/src/main/java/org/sid/securityservice/web/AuthController.java
@@ -1,14 +1,20 @@
 package org.sid.securityservice.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,22 +23,59 @@ import java.util.stream.Collectors;
 public class AuthController {
 
     private JwtEncoder jwtEncoder;
+    private JwtDecoder jwtDecoder;
+    private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
 
-    public AuthController(JwtEncoder jwtEncoder) {
+    public AuthController(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder, AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
         this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/token")
-    private Map<String, String> jwtToken(Authentication authentication){
+    private ResponseEntity<Map<String, String>> jwtToken(
+            String grantType,
+            String username,
+            String password,
+            Boolean withRefreshToken,
+            String refreshToken){
+        String subject = null;
+        String scope = null;
+        if(grantType.equals("password")) {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            subject = authentication.getName();
+            scope = authentication.getAuthorities()
+                    .stream().map(aut -> aut.getAuthority())
+                    .collect(Collectors.joining(" "));
+
+        }else if (grantType.equals("refreshToken")){
+            if(refreshToken==null) {
+                return new ResponseEntity<>(Map.of("errorMessage", "Refresh Token is required"),
+                        HttpStatus.UNAUTHORIZED);
+            }
+            Jwt decodeJWT = null;
+            try {
+                decodeJWT = jwtDecoder.decode(refreshToken);
+            } catch (JwtException e) {
+                return new ResponseEntity<>(Map.of("errorMessage", e.getMessage()), HttpStatus.UNAUTHORIZED);
+            }
+            subject= decodeJWT.getSubject();
+            UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
+            Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+            scope= authorities.stream().map(auth-> auth.getAuthority()).collect(Collectors.joining(" "));
+        }
+
+
         Map<String, String> idToken = new HashMap<>();
         Instant instant = Instant.now();
-        String scope = authentication.getAuthorities()
-                .stream().map(aut -> aut.getAuthority())
-                .collect(Collectors.joining(" "));
         JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-                .subject(authentication.getName())
+                .subject(subject)
                 .issuedAt(instant)
-                .expiresAt(instant.plus(5, ChronoUnit.MINUTES))
+                .expiresAt(instant.plus(withRefreshToken?1: 5, ChronoUnit.MINUTES))
                 .issuer("security-service")
                 .claim("scope", scope)
                 .build();
@@ -40,6 +83,18 @@ public class AuthController {
         //Jwt Encoder qui permet de generer le JWT Token
         String jwtAccessToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
         idToken.put("accessToken", jwtAccessToken);
-        return idToken ;
+        if(withRefreshToken){
+            JwtClaimsSet jwtClaimsSetRefresh = JwtClaimsSet.builder()
+                    .subject(subject)
+                    .issuedAt(instant)
+                    .expiresAt(instant.plus(5, ChronoUnit.MINUTES))
+                    .issuer("security-service")
+                    .build();
+
+            //Jwt Encoder qui permet de generer le JWT Token
+            String jwtRefreshToken = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSetRefresh)).getTokenValue();
+            idToken.put("refreshToken", jwtRefreshToken);
+        }
+        return new ResponseEntity<>(idToken, HttpStatus.OK) ;
     }
 }
